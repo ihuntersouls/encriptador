d6f3ec75e3f0fec8bdde09685560369b947f797e
ihuntersouls <neiderzamoramoreno@gmail.com>
2022-08-14 14:40:09 -0500

    funcion copar texto agregada

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Funcion encriptar */
 function encriptar (){
     let texto = document.querySelector("#texto").value;
     let textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
@@ -8,6 +9,7 @@ function encriptar (){
     let btnEncriptar = document.querySelector("#encriptarBoton"); 
     btnEncriptar.onclick = encriptar;
 
+/* Funcion desencriptar */
 function desencriptar (){ 
     let texto = document.querySelector("#texto").value; 
     let textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u"); 
@@ -17,3 +19,20 @@ function desencriptar (){
 
     let btnDesencriptar = document.querySelector("#desencriptarBoton"); 
     btnDesencriptar.onclick = desencriptar;
+
+/*Funcion copiar */
+let copyTextareaBtn = document.querySelector('#botonCopiar');
+
+copyTextareaBtn.addEventListener('click', function(event) {
+  let copyTextarea = document.querySelector('#textoEncriptado');
+  copyTextarea.focus();
+  copyTextarea.select();
+
+  try {
+    let successful = document.execCommand('copy');
+    let msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+});
